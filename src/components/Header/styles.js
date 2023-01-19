d07c21b240169808552ff67d96f241ff5e4d2a92
JoaoVDS21 <joao.programmerS2@gmail.com>
2023-01-18 23:04:34 -0300

    Inicialização projeto

---
+++ src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  margin: 74px 0 48px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
