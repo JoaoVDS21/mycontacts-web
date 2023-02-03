c6f9846b93e78ce30182eade174b1c9db66a7482
JoaoVDS21 <joao.programmerS2@gmail.com>
2023-02-02 23:54:52 -0300

    Implementação useCallback

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,22 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 import { Link } from "react-router-dom";
 
-import { Container, InputSearchContainer, Header, ListHeader, Card } from "./styles";
+import {
+  Container,
+  InputSearchContainer,
+  Header,
+  ListHeader,
+  Card,
+  ErrorContainer
+} from "./styles";
 
 import arrow from '../../assets/images/icons/arrow.svg'
 import edit from '../../assets/images/icons/edit.svg'
 import trash from '../../assets/images/icons/trash.svg'
+import sad from '../../assets/images/icons/sad.svg'
 
 import Loader from "../../components/Loader";
+import Button from '../../components/Button';
 
 import ContactsService from "../../services/ContactsService";
 import APIError from "../../errors/APIError";
@@ -17,6 +26,7 @@ export default function Home() {
   const [orderBy, setOrderBy] = useState('desc')
   const [searchTerm, setSearchTerm] = useState('')
   const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   const filteredContacts = useMemo(() => {
     return contacts.filter(contact => (
@@ -24,28 +34,26 @@ export default function Home() {
     ))
   }, [contacts, searchTerm])
 
-  useEffect(() => {
-    async function loadContacts() {
-      try {
-        setIsLoading(true);
+  // useCallback, só cria a função no primeiro render ou de acordo com o array de dependencias dele
+  const loadContacts = useCallback(async() => {
+    try {
+      setIsLoading(true);
 
-        const contactsList = await ContactsService.listContacts(orderBy);
+      const contactsList = await ContactsService.listContacts(orderBy);
 
-        setContacts(contactsList);
+      setContacts(contactsList);
+      setHasError(false);
 
-      } catch (error){
-        console.log("Name: ", error.name)
-        console.log("Message: ", error.message)
-        console.log("Response: ", error.response)
-      } finally {
-        setIsLoading(false);
-      }
+    } catch (error){
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
     }
-
-    loadContacts();
-
-    return () => console.log('cleanup');
   }, [orderBy])
+
+  useEffect(() => {
+    loadContacts();
+  }, [loadContacts])
 
   function handleToggleOrderBy() {
     setOrderBy(prevState => (
@@ -55,6 +63,10 @@ export default function Home() {
 
   function handleChangeSearchTerm(event) {
     setSearchTerm(event.target.value)
+  }
+
+  function handleTryAgain() {
+    loadContacts();
   }
 
   return (
@@ -70,45 +82,67 @@ export default function Home() {
         />
       </InputSearchContainer>
 
-      <Header>
-        <strong>
-          {filteredContacts.length}
-          {filteredContacts.length === 1 ? ' contato' : ' contatos'}
-        </strong>
+      <Header hasError={hasError}>
+        {!hasError && (
+          <strong>
+            {filteredContacts.length}
+            {filteredContacts.length === 1 ? ' contato' : ' contatos'}
+          </strong>
+        )}
         <Link to="/new">Novo contato</Link>
       </Header>
 
-      {filteredContacts.length > 0 && (
-        <ListHeader orderBy={orderBy}>
-          <button type="button" onClick={handleToggleOrderBy}>
-            <span>Nome</span>
-            <img src={arrow} alt="Arrow" />
-          </button>
-        </ListHeader>
+      {hasError && (
+        <ErrorContainer>
+          <img src={sad} alt="" />
+
+          <div className="details">
+            <strong>Ocorreu um erro ao obter os seus contatos!</strong>
+            <Button
+              onClick={handleTryAgain}
+              type="button"
+            >
+              Tentar novamente
+            </Button>
+          </div>
+        </ErrorContainer>
       )}
 
-      {filteredContacts.map((contact) => (
-        <Card key={contact.id}>
-          <div className="info">
-            <div className="contact-name">
-              <strong>{contact.name}</strong>
-              {contact.category_name && (
-                <small>{contact.category_name}</small>
-              )}
-            </div>
-            <span>{contact.email}</span>
-            <span>{contact.phone}</span>
-          </div>
-          <div className="actions">
-            <Link to={`/edit/${contact.id}`}>
-              <img src={edit} alt="Edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="Delete" />
-            </button>
-          </div>
-        </Card>
-      ))}
+      {!hasError && (
+        <>
+          {filteredContacts.length > 0 && (
+            <ListHeader orderBy={orderBy}>
+              <button type="button" onClick={handleToggleOrderBy}>
+                <span>Nome</span>
+                <img src={arrow} alt="Arrow" />
+              </button>
+            </ListHeader>
+          )}
+
+          {filteredContacts.map((contact) => (
+            <Card key={contact.id}>
+              <div className="info">
+                <div className="contact-name">
+                  <strong>{contact.name}</strong>
+                  {contact.category_name && (
+                    <small>{contact.category_name}</small>
+                  )}
+                </div>
+                <span>{contact.email}</span>
+                <span>{contact.phone}</span>
+              </div>
+              <div className="actions">
+                <Link to={`/edit/${contact.id}`}>
+                  <img src={edit} alt="Edit" />
+                </Link>
+                <button type="button">
+                  <img src={trash} alt="Delete" />
+                </button>
+              </div>
+            </Card>
+          ))}
+        </>
+      )}
     </Container>
   );
 }
--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -6,7 +6,7 @@ class ContactsService {
   }
 
   async listContacts(orderBy = 'asc') {
-    return this.httpClient.get(`/contacts/043cb5ce-bbdb-4884-a5ad-cca2e1c5748c?orderBy=${orderBy}`);
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
 
   async createContacts(contact) {
