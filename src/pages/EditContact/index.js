5539d5046c24af64fb6b5a54776524b25c2a78ad
João VDS <joao.programmerS2@gmail.com>
2023-03-08 12:34:58 -0300

    Trazendo dados para edição de contato

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
 import ContactForm from "../../components/ContactForm";
 import PageHeader from "../../components/PageHeader";
+import ContactsService from "../../services/ContactsService";
+
+import Loader from "../../components/Loader";
+import toast from "../../utils/toast";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        console.log(contactData);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!'
+        })
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
   function handleSubmit(formData) {
     console.log('EditContacts', { formData })
   }
 
   return (
     <>
+      <Loader isLoading={isLoading}/>
+
       <PageHeader
         title='Editar Matheus Silva'
       />
